bdb9803fbfea519f5d6cf60aaae5b0b84465e705
NB1723 <notalia03@gmail.com>
2020-11-04 17:53:13 +0200

    first master commit

---
+++ lab5.1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+double k(const double x, const double y);
+int main()
+{
+	double p,q;
+	cout<<"p=";cin>>p;
+	cout<<"q=";cin>>q;
+	double d=(k(1+p*q,q*q)+((k(p,p*p))*(k(p,p*p)))/(1+k(p*q+q*q,p)));
+	cout<<"d="<<d<<endl;
+	return 0;
+}
+double k(const double x, const double y)
+{
+	return ((x/(1+sin(y)*sin(y)))+(y/(1+x*x)));
+}
